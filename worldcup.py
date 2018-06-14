6076c1850020d0600beb25840fc308447101fdc1
raulbarrue <raulbarrue@outlook.com>
2018-06-14 21:49:03 +0100

    retrieve game data

--- worldcup.py
+++ worldcup.py
@@ -1,14 +1,40 @@
 import requests
 
-r = requests.get('http://api.football-data.org/v1/competitions/467/fixtures')
+def get_data(url):
+    ''' Returns World Cup 2018 data in JSON format'''
+    r = requests.get(url)
+    return r.json()
 
-data = r.json()
+def get_results(data, game):
+    gameData = data["fixtures"][game]
 
-match = 0
-matchData = data["fixtures"][match]
-awayTeam = matchData["awayTeamName"]
-homeTeam = matchData["homeTeamName"]
-goalsHome = matchData["result"]["goalsHomeTeam"]
-goalsAway = matchData["result"]["goalsAwayTeam"]
+    results = {}    
 
-print(homeTeam, goalsHome, '-', goalsAway, awayTeam)
+    homeTeam = gameData["homeTeamName"]
+    results["homeTeam"] = homeTeam
+
+    goalsHome = gameData["result"]["goalsHomeTeam"]
+    results["goalsHome"] = goalsHome
+
+    awayTeam = gameData["awayTeamName"]
+    results["awayTeam"] = awayTeam
+
+    goalsAway = gameData["result"]["goalsAwayTeam"]
+    results["goalsAway"] = goalsAway
+
+    return results 
+
+
+def main():
+    url = 'http://api.football-data.org/v1/competitions/467/fixtures'
+    data = get_data(url)
+    n_games = data["count"]
+
+    for game in n_games:
+    
+    results = get_results(data, 0)
+    
+    
+
+if __name__ == '__main__':
+    main()
